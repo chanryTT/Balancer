6dfe2eb84332308300777f29532f1c2b4d8d5705
Developer <developer@balancesystem.dev>
2026-07-07 13:15:50 +0800

    feat: add BalancingTestView with step wizard and results display

---
+++ src/BalanceSystem.App/Views/BalancingTestView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace BalanceSystem.App.Views;
+
+public partial class BalancingTestView : UserControl
+{
+    public BalancingTestView()
+    {
+        InitializeComponent();
+    }
+}
